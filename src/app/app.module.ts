f5f43f9f3cde6a3cd9bbd8eb2824d2e64b0d2ce1
Yuta <>
2020-08-10 20:23:29 +0900

    Angularフロントエンド側作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
---
+++ src/app/products.ts
@@ -0,0 +1,50 @@
+export const products = [
+    {
+        name: 'Phone XL',
+        price: 799,
+        description: 'A large phone with one of the best screens',
+        coverImage: './assets/img/phone-cover.jpg',
+        heading1: 'hogehoge1',
+        heading2: 'hogehoge2',
+        heading3: 'hogehoge3',
+        headtext1: 'hugahuga1',
+        headtext2: 'hugahuga2',
+        headtext3: 'hugahuga3'
+    },
+    {
+        name: 'Phone Mini',
+        price: 699,
+        description: 'A great phone with one of the best cameras',
+        coverImage: './assets/img/phone-cover.jpg',
+        heading1: 'hogehoge1',
+        heading2: 'hogehoge2',
+        heading3: 'hogehoge3',
+        headtext1: 'hugahuga1',
+        headtext2: 'hugahuga2',
+        headtext3: 'hugahuga3'
+    },
+    {
+        name: 'Phone Standard',
+        price: 299,
+        description: '',
+        coverImage: './assets/img/phone-cover.jpg',
+        heading1: 'hogehoge1',
+        heading2: 'hogehoge2',
+        heading3: 'hogehoge3',
+        headtext1: 'hugahuga1',
+        headtext2: 'hugahuga2',
+        headtext3: 'hugahuga3'
+    },
+    {
+        name: 'Phone Special',
+        price: 999,
+        description: '',
+        coverImage: './assets/img/phone-cover.jpg',
+        heading1: 'hogehoge1',
+        heading2: 'hogehoge2',
+        heading3: 'hogehoge3',
+        headtext1: 'hugahuga1',
+        headtext2: 'hugahuga2',
+        headtext3: 'hugahuga3'
+    }
+];
