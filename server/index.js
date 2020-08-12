0b75d9cd0a83b3f37005edb2c6986b9b081f20e1
Yuta <>
2020-08-12 23:04:45 +0900

    herocu用セットアップ

--- server/index.js
+++ server/index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const config = require('./config/dev');
+const config = require('./config');
 const FakeDb = require('./fake-db');
 
 const productRoutes = require('./routes/products');
+const path = require('path');
 
-// await mongoose.connect('mongodb+srv://test:testtest@cluster0.mu52v.mongodb.net/<dbname>?retryWrites=true&w=majority', {
 mongoose.connect(config.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(
 	() => {
-		const fakeDb = new FakeDb();
-		fakeDb.initDb();
+		if (process.env.NODE_ENV !== 'production') {
+			const fakeDb = new FakeDb();
+			// fakeDb.initDb();
+		}
 	}
 );
 
@@ -20,9 +22,14 @@ const app = express();
 
 app.use('/api/v1/products', productRoutes);
 
-// app.get('/products', function (req, res) {
-// 	res.json({'success': true })
-// })
+if (process.env.NODE_ENV === 'production') {
+	const appPath = path.join(__dirname, '..', 'dist', 'reserve-system')
+	app.use(express.static(appPath));
+	app.get("*", function (req, res) {
+		res.sendFile(path.resolve(appPath, 'index.html'));
+	})
+}
+
 const PORT = process.env.PORT || '3001'
 
 app.listen( PORT, function () { 
